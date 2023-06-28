eb92c36a04f09ef8693ae7a6047dada8e90293f6
sathiranipun <sathiranipun.vsn@gmail.com>
2023-06-28 17:44:09 +0530

    version 3 updated

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// 1. Grab the box from the DOM and store it in a variable
-// 2. Add a click event listener to the box 
-// 3. Log out "I want to open the box!" when it's clicked
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-let box = document.getElementById("box")
-
-box.addEventListener("click", function(){
-    console.log("I want to open the box!")
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
