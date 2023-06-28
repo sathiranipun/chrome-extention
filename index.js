4efa37f8074dc01d93197a399a145647d3eb56b0
sathiranipun <sathiranipun.vsn@gmail.com>
2023-06-28 16:29:49 +0530

    upgrade v2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+// 1. Grab the box from the DOM and store it in a variable
+// 2. Add a click event listener to the box 
+// 3. Log out "I want to open the box!" when it's clicked
+
+let box = document.getElementById("box")
+
+box.addEventListener("click", function(){
+    console.log("I want to open the box!")
+})
